22cf8ce7a4013c6194663a394cd744457e90d669
petenut <min.sb.kim@gmail.com>
2016-06-24 00:51:47 -0700

    changes to admin message for signing in and removes email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :venues, through: :visits
   
   validates :first_name, :last_name, :description, :age, :email, :current_city, :password, presence: true
-  validates :email_confirmation, :password_confirmation, presence: true
+  validates :password_confirmation, presence: true
   validates :email, uniqueness: true
 
   has_secure_password
